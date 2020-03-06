df87ae9546c344cd5a6d24b615279774c3962cf0
sajtizsolt <sajtizsolt@gmail.com>
2020-03-06 23:37:56 +0100

    Created Receipty entity

---
+++ back-end/fairshare/src/main/java/hu/elte/fairshare/entities/Receipt.java
@@ -0,0 +1,59 @@
+package hu.elte.fairshare.entities;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+/**
+ * The Receipt class implements a receipt of a user (group).
+ * 
+ * @author sajtizsolt
+ */
+@Data
+@Entity
+@Table(name = "Receipt", schema = "public")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Receipt {
+    
+    /**
+     * The unique id of the receipt.
+     */
+    @Id
+    @Column(name = "id", updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    /**
+     * The time the record was created.
+     */
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    /**
+     * The time the record was last updated.
+     */
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    /**
+     * The set of users attached to the receipt.
+     */
+    @ManyToMany(mappedBy = "receipts")
+    private Set<User> users;
+}
