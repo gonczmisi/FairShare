54d0d06e303f3006e191af1b9510d12d7084626c
sajtizsolt <sajtizsolt@gmail.com>
2020-04-09 13:44:29 +0200

    Finally added tests

--- back-end/fairshare/src/test/java/hu/elte/fairshare/FairshareApplicationTests.java
+++ back-end/fairshare/src/test/java/hu/elte/fairshare/FairshareApplicationTests.java
@@ -3,11 +3,10 @@ package hu.elte.fairshare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FairShareApplication.class)
 class FairshareApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    
+    @Test
+    void contextLoads() {
+    }
 }
