ffcb82067b2ef145024da7b30714e0c03134751a
rizstej <novaklilla0621@gmail.com>
2020-05-17 01:49:56 +0200

    Added static Receipt and Item forms

--- front-end/fairshare/src/AddItems.js
+++ front-end/fairshare/src/AddItems.js
@@ -1,14 +1,90 @@
 import React from 'react';
 import './App.css';
 
-function AddItems() {
-  return (
-    <div>
-      
-      <h1>Add Items</h1>
+
+class AddItems extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: 1,
+          created_at: '',
+          updated_at: '',
+          name: '',
+          price: null,
+          currency_type: 'HUF',
+          
+          date: new Date(),
+          options: { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric',  minute: 'numeric'},
+        };
         
-    </div>
-  );
-}
+        this.handleTxtChange = this.handleTxtChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleTxtChange(event) {
+      let name = event.target.name;
+      let value = event.target.value;
+      this.setState({[name]: value});
+    }
+
+    handleChange(event) {
+      this.setState({currency_type: event.target.value});
+    }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.setState({id: this.state.id+1});
+
+      var tmp_date = this.state.date.toLocaleDateString('en-GB', this.state.options);
+      this.setState({created_at: tmp_date});
+      this.setState({updated_at: tmp_date});
+      this.setState({name: event.target.name});
+      this.setState({price: event.target.price});
+      
+      this.setState({name: ''});
+      this.setState({price: null});
+    }  
+
+    
+    render() {
+        return (
+            <div>
+                <h1>Add Items</h1>
+                <p> Fill out the fields to add an item! </p>
+                <br></br>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Item name:</label>
+                        <input type="text" className="form-control" placeholder="Item name" name="name" required onChange={this.handleTxtChange}/>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Price:</label>
+                        <input type="number" className="form-control" placeholder="Price" name="price" required onChange={this.handleTxtChange}/>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className="form-group">
+                    <label>Currency Type:
+                        <select value={this.state.currency_type} onChange={this.handleChange}>
+                            <option currency_type="HUF">[HUF] Hungarian Forint</option>
+                            <option currency_type="USD">[USD] American Dollar</option>
+                            <option currency_type="EUR">[EUR] Euro</option>
+                        </select>
+                    </label> 
+                    </div>
+                    <br></br>
+                    <br></br>                 
+                                       
+                    <button type="submit" className="btn btn-primary">ADD</button>
+                </form>
+            </div>
+        );
+    }
+ }
 
 export default AddItems;
--- front-end/fairshare/src/AddReceipts.js
+++ front-end/fairshare/src/AddReceipts.js
@@ -1,14 +1,68 @@
 import React from 'react';
 import './App.css';
 
-function AddReceipts() {
-  return (
-    <div>
-      
-      <h1>Add Receipts</h1>
+
+class AddReceipts extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: 1,
+          created_at: '',
+          updated_at: '',
+          name: '',
+          
+          date: new Date(),
+          options: { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric',  minute: 'numeric'},
+        };
         
-    </div>
-  );
-}
+        this.handleTxtChange = this.handleTxtChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleTxtChange(event) {
+      let name = event.target.name;
+      let value = event.target.value;
+      this.setState({[name]: value});
+    }
+
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.setState({id: this.state.id+1});
+
+      var tmp_date = this.state.date.toLocaleDateString('en-GB', this.state.options);
+      this.setState({created_at: tmp_date});
+      this.setState({updated_at: tmp_date});
+      this.setState({name: event.target.name});
+      this.setState({price: event.target.price});
+      
+      this.setState({name: ''});
+      this.setState({price: null});
+    }  
+
+    
+    render() {
+        return (
+            <div>
+                <h1>Add Recepits</h1>
+                <p> Fill out the fields to add a receipt! </p>
+                <br></br>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Recepit name:</label>
+                        <input type="text" className="form-control" placeholder="Receipt name" name="name" required onChange={this.handleTxtChange}/>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <p>select items [to be added]</p>                 
+                                       
+                    <button type="submit" className="btn btn-primary">ADD</button>
+                </form>
+            </div>
+        );
+    }
+ }
 
 export default AddReceipts;
