6dc630d12465ebd3d30ef3c97260630837a4f8b5
gonczmisi <gonczmisi@gmail.com>
2020-05-17 21:44:01 +0200

    Added basic authentication in frontend with context

--- front-end/fairshare/src/App.js
+++ front-end/fairshare/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Nav from './Nav';
 import AddReceipts from './AddReceipts';
@@ -7,24 +7,40 @@ import AddItems from './AddItems';
 import Items from './Items';
 import Login from './Login';
 import Register from './Register';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthProvider } from "./context/Auth";
 
 function App() {
+  const existingTokens = JSON.parse(sessionStorage.getItem("currentUser"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    if (data) {
+      sessionStorage.setItem('currentUser', JSON.stringify(data));
+    } else {
+      sessionStorage.clear();
+    }
+
+    setAuthTokens(data);
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/add-receipts" component={AddReceipts} />
-          <Route path="/receipts" component={Receipts} />
-          <Route path="/add-items" component={AddItems} />
-          <Route path="/items" component={Items} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </div>
-    </Router>
+    <AuthProvider value={{ authTokens: authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/add-receipts" component={AddReceipts} />
+            <Route path="/receipts" component={Receipts} />
+            <Route path="/add-items" component={AddItems} />
+            <Route path="/items" component={Items} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- front-end/fairshare/src/Login.js
+++ front-end/fairshare/src/Login.js
@@ -1,58 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { useAuth } from './context/Auth';
 
-class Login extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
+const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_VM_URL = 'http://137.117.229.78:8080';
+const USER_URL = BACKEND_URL + '/user';
+const LOGIN_URL = BACKEND_URL + '/login';
 
-        this.state =
-        {
-            id: '',
-            username: '',
-            password: '',
-            email_address: '',
-            success: null,
-        };
-    }
-
+export default function Login() {
+    const { setAuthTokens } = useAuth();
     
-    render()
-    {
-        return (
-            <div>
-                <h1>Login</h1>
-                <p>
-                    Please fill out the required fields to log in!
-                </p>
-                <br></br>
-                
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label>Username:      </label>
-                        <input type="text" className="form-control" placeholder="Username" name="username" required />
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <div className="form-group">
-                        <label>E-Mail Address:      </label>
-                        <input type="text" className="form-control" placeholder="E-Mail Address" name="username" required />
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <div className="form-group">
-                        <label>Password:      </label>
-                        <input type="password" className="form-control" placeholder="Password" name="newPassword" required />
-                    </div>
-                    <br></br>
-                    <br></br>                 
-                                       
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
-            </div>
-        );
-    }
- }
+    const [id, setId] = useState();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [email, setEmail] = useState();
+    const [success, setSuccess] = useState(null);
 
-export default Login;
+    function handleClick(event) {
+        console.log(username, password)
+        console.log(LOGIN_URL);
+        axios.post(
+            LOGIN_URL,
+            {},
+            {
+                auth: {
+                    username: username,
+                    password: password
+                }
+            }
+        )
+            .then(function (response) {
+                console.log(response);
+                if (response.status === 200) {
+                    console.log("Login successfull");
+                    response.data.password = password;
+                    setAuthTokens(response.data);
+                    setSuccess(true);
+                } else{
+                    setSuccess(false);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                setSuccess(false);
+            });
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <p>
+                Please fill out the required fields to log in!
+                </p>
+            <br></br>
+
+            <form onSubmit={handleClick}>
+                <div className="form-group">
+                    <label>Username:      </label>
+                    <input type="text" className="form-control" placeholder="Username" name="username" required />
+                </div>
+                <br></br>
+                <br></br>
+                <div className="form-group">
+                    <label>E-Mail Address:      </label>
+                    <input type="text" className="form-control" placeholder="E-Mail Address" name="username" required />
+                </div>
+                <br></br>
+                <br></br>
+                <div className="form-group">
+                    <label>Password:      </label>
+                    <input type="password" className="form-control" placeholder="Password" name="newPassword" required />
+                </div>
+                <br></br>
+                <br></br>
+
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+        </div>
+    );
+}
