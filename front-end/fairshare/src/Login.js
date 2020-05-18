59f402f5834a24be462fc4dee51e97b79741cc40
gonczmisi <gonczmisi@gmail.com>
2020-05-18 19:31:56 +0200

    Added working login feature

--- front-end/fairshare/src/Login.js
+++ front-end/fairshare/src/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 import { useAuth } from './context/Auth';
+import { Redirect } from 'react-router-dom';
 
 const BACKEND_URL = 'http://localhost:8080';
 const BACKEND_VM_URL = 'http://137.117.229.78:8080';
@@ -10,14 +12,16 @@ const LOGIN_URL = BACKEND_URL + '/login';
 
 export default function Login() {
     const { setAuthTokens } = useAuth();
-    
-    const [id, setId] = useState();
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const [email, setEmail] = useState();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [success, setSuccess] = useState(null);
 
-    function handleClick(event) {
+    function validateForm() {
+        return password.length > 0 && username.length > 0;
+    }
+
+    function handleSubmit(event) {
         console.log(username, password)
         console.log(LOGIN_URL);
         axios.post(
@@ -37,7 +41,7 @@ export default function Login() {
                     response.data.password = password;
                     setAuthTokens(response.data);
                     setSuccess(true);
-                } else{
+                } else {
                     setSuccess(false);
                 }
             })
@@ -47,36 +51,32 @@ export default function Login() {
             });
     };
 
+    if(success){
+        return <Redirect to="/receipts" />
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-            <p>
-                Please fill out the required fields to log in!
-                </p>
-            <br></br>
-
-            <form onSubmit={handleClick}>
-                <div className="form-group">
-                    <label>Username:      </label>
-                    <input type="text" className="form-control" placeholder="Username" name="username" required />
-                </div>
-                <br></br>
-                <br></br>
-                <div className="form-group">
-                    <label>E-Mail Address:      </label>
-                    <input type="text" className="form-control" placeholder="E-Mail Address" name="username" required />
-                </div>
-                <br></br>
-                <br></br>
-                <div className="form-group">
-                    <label>Password:      </label>
-                    <input type="password" className="form-control" placeholder="Password" name="newPassword" required />
-                </div>
-                <br></br>
-                <br></br>
-
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
+        <div className="Login">
+            <FormGroup controlId="username" bsSize="large">
+                Username
+                    <FormControl
+                    autoFocus
+                    type="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+                Password
+                    <FormControl
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                />
+            </FormGroup>
+            <Button block bsSize="large" disabled={!validateForm()} onClick={handleSubmit} >
+                Login
+                </Button>
         </div>
     );
 }
